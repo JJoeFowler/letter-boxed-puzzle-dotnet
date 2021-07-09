3e779c15751f332aad2208683c7e4c7d8b9fa1fc
Joe Fowler <JJoeFowler@gmail.com>
2021-07-08 17:56:02 -0700

    Added extra unit tests for ToFirstCharUpper() string extension method.

--- tests/LetterBoxedPuzzle.Framework.Tests.Unit/StringExtensionsTests.cs
+++ tests/LetterBoxedPuzzle.Framework.Tests.Unit/StringExtensionsTests.cs
@@ -7,11 +7,12 @@
 
 namespace LetterBoxedPuzzle.Framework.Tests.Unit
 {
+    using System;
     using System.Linq;
 
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using LetterBoxedPuzzle.Framework.Extensions;
 
-    using static Extensions.StringExtensions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     using static TestCommonConstants;
 
@@ -21,6 +22,65 @@ namespace LetterBoxedPuzzle.Framework.Tests.Unit
     [TestClass]
     public class StringExtensionsTests
     {
+        /// <summary>
+        ///     The test strings with the first letter lowercased.
+        /// </summary>
+        private static readonly string[] TestFirstCharLowercasedStrings = { "abc", "d ef", "gH i", "jKL", "mn  O", "p qR ", "sTU", "vwX", "yZ " };
+
+        /// <summary>
+        ///     Corresponding test strings with the first letter uppercased.
+        /// </summary>
+        private static readonly string[] TestFirstCharUppercasedStrings = { "Abc", "D ef", "GH i", "JKL", "Mn  O", "P qR ", "STU", "VwX", "YZ " };
+
+        /// <summary>
+        ///     Verify whether given a null value throws an argument null exception.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ToFirstCharUpper_GivenNullParameters_ThrowsArgumentNullException()
+        {
+            // Act, disabling warning of passing a null value for a non-nullable string parameter.
+#pragma warning disable 8625
+            _ = StringExtensions.ToFirstCharUpper(null);
+#pragma warning restore 8625
+        }
+
+        /// <summary>
+        ///     Verify whether given an empty throws an argument exception.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ToFirstCharUpper_GivenEmptyString_ThrowsArgumentNullException()
+        {
+            // Act
+            _ = string.Empty.ToFirstCharUpper();
+        }
+
+        /// <summary>
+        ///     Verifies whether given a lowercase letter is converted to the corresponding uppercase letter.
+        /// </summary>
+        [TestMethod]
+        public void ToFirstCharUpper_LowercaseLetter_ConvertsToCorrespondingUppercaseLetter()
+        {
+            // Arrange
+            var expectedLetters = UppercaseTestAlphabet.Select(letter => letter.ToString()).ToArray();
+
+            // Act
+            var actualLetters = LowercaseTestAlphabet.Select(letter => letter.ToString().ToFirstCharUpper()).ToArray();
+
+            // Assert
+            for (var letterIndex = 0; letterIndex < AlphabetSize; letterIndex++)
+            {
+                var lowercaseLetter = LowercaseTestAlphabet[letterIndex].ToString();
+                var expectedLetter = expectedLetters[letterIndex];
+                var actualLetter = actualLetters[letterIndex];
+                Assert.AreEqual(
+                    expectedLetter,
+                    actualLetter,
+                    $"Expected the lowercase '{lowercaseLetter}' to be converted to '{expectedLetter}' and not '{actualLetter}'.");
+            }
+        }
+
         /// <summary>
         ///     Verify whether given the test strings with the first character lowercased are converted to the corresponding test
         ///     strings with the first character uppercased.
