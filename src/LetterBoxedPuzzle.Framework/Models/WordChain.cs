e74a3ead9cde757a309f4fce6152129c60598dae
Joe Fowler <JJoeFowler@gmail.com>
2021-07-09 19:20:16 -0700

    Initial word chain class for constructing chain of word links from specified linking letters.

---
+++ src/LetterBoxedPuzzle.Framework/Models/WordChain.cs
@@ -0,0 +1,90 @@
+﻿// ===============================================================================================================================================
+// <copyright file="WordChain.cs" company="Joe Fowler">
+// Copyright (c) 2021 Joe Fowler.
+// Licensed under the GNU Affero General Public License v3. See LICENSE file in the project root for full license information.
+// </copyright>
+// ===============================================================================================================================================
+
+namespace LetterBoxedPuzzle.Framework.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using static Utilities.AlphabetUtilities;
+
+    /// <summary>
+    ///     Class for a word chain, consisting of the chain of candidate words where each subsequent word letter starts with the letter last
+    ///     letter of the previous word in the chain.
+    /// </summary>
+    public class WordChain
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="WordChain" /> class.
+        /// </summary>
+        /// <param name="candidateWords">The candidate words.</param>
+        /// <param name="linkingLetters">The linking letters.</param>
+        /// <exception cref="ArgumentNullException">Thrown when given a null value for any of the parameters.</exception>
+        public WordChain(CandidateWord[] candidateWords, IEnumerable<char> linkingLetters)
+        {
+            (this.CandidateWords, this.LinkingLetters) = (candidateWords, linkingLetters) switch
+                {
+                    (null, _) => throw new ArgumentNullException(nameof(candidateWords)),
+
+                    (_, null) => throw new ArgumentNullException(nameof(linkingLetters)),
+
+                    (_, _) when candidateWords.Length == 0 => throw new ArgumentException($"'{nameof(candidateWords)}' cannot be empty."),
+
+                    (_, _) when linkingLetters.All(IsAlphabetLetter) => throw new ArgumentException(
+                        $"'{nameof(linkingLetters)}' must only be letters of the alphabet"),
+
+                    (_, _) => (candidateWords, linkingLetters.ToArray()),
+                };
+
+            this.WordLinks = GenerateWordLinks(candidateWords, linkingLetters);
+        }
+
+        /// <summary>
+        ///     Gets the candidate words.
+        /// </summary>
+        public CandidateWord[] CandidateWords { get; }
+
+        /// <summary>
+        ///     Gets the linking letters, which are the last letters of the previous words in the chain that match the first letters of the
+        ///     next words.
+        /// </summary>
+        public char[] LinkingLetters { get; }
+
+        /// <summary>
+        ///     Gets the words links for the word chain.
+        /// </summary>
+        public WordLink[] WordLinks { get; }
+
+        /// <summary>
+        ///     Generate the words links for the specified linking letters using the specified candidate words.
+        /// </summary>
+        /// <returns>The words for the specified candidate words based upon the specified linking letters.</returns>
+        /// <param name="candidateWords">The candidate words.</param>
+        /// <param name="linkingLetters">The linking letters.</param>
+        private static WordLink[] GenerateWordLinks(CandidateWord[] candidateWords, IEnumerable<char> linkingLetters) =>
+            linkingLetters.Count() switch
+                {
+                    0 => new WordLink[] { new (candidateWords, null, null) },
+                    _ => GenerateLinkingLetterPairs(linkingLetters).Select(pair => new WordLink(candidateWords, pair.Current, pair.Next)).ToArray(),
+                };
+
+        /// <summary>
+        ///     Generates sequence of nullable character pairs from the linking letters, where the first value of the first pair is null
+        ///     and the second value of last pair is also null.
+        /// </summary>
+        /// <param name="linkingLetters">The linking letters.</param>
+        /// <returns>The sequential pairs of characters of the linking letters.</returns>
+        private static IEnumerable<(char? Current, char? Next)> GenerateLinkingLetterPairs(IEnumerable<char> linkingLetters)
+        {
+            var nullCharacter = new char?[] { null };
+            var nullableLinkingLetters = linkingLetters.Select(letter => (char?)letter);
+
+            return nullCharacter.Concat(nullableLinkingLetters).Zip(nullableLinkingLetters.Concat(nullCharacter)).ToArray();
+        }
+    }
+}
